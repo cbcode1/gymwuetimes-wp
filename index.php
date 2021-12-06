b2494bfc552c344edd13a0beb0c31e438efd3f35
cbcode1 <cbcode@outlook.com>
2021-12-06 20:58:38 +0100

    banner support

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 <?php get_header(); ?>
 
+    <?php
+        $args =  array(
+            'post_type' => 'ausgaben',
+            'orderby' => 'title',
+            'order' => 'DESC',
+            'posts_per_page' => 1,
+        );
+
+        $custom_query = new WP_Query( $args );
+        while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
+        <a class="banner" href="<?php the_permalink() ?>">
+                <img src="<?php the_post_thumbnail_url(); ?>" alt="Cover von <?php the_title(); ?>" title="Cover von <?php the_title(); ?>">
+
+                <div class="banner__text">
+                    <h2 class="banner__ueberschrift"><?php the_title(); ?> ist da!</h2>
+                    <?php the_content(); ?>
+                </div>
+        </a>
+            
+    <?php endwhile; ?>
+
     <ul class="ressorts">
         <?php wp_list_categories( '&title_li=' ); ?>
     </ul>
+
     <ul class="artikelliste">
         <?php
             if ( have_posts() ) : 
