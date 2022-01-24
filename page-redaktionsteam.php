d1d1c6782fd522d28f2d0596f92cbf420935b1af
cbcode1 <cbcode@outlook.com>
2022-01-24 16:07:26 +0100

    author pictures

--- page-redaktionsteam.php
+++ page-redaktionsteam.php
@@ -11,11 +11,12 @@
         <h2>Redaktion</h2>
         <ul class="artikelliste">
 
-            <?php $authors = get_users();
+            <?php $authors = get_multiple_authors();
             foreach($authors as $author) { ?>
                 <a href="/author/<?php the_author_meta("nickname", $author->ID) ?>" class="artikelliste__link">
                     <div class="artikelliste__kasten">
                         <div class="artikelliste__div">
+                            <img src="<?php $author->get_avatar_url() ?>" />
                             <h2 class="artikelliste__ueberschrift"><?php the_author_meta("display_name", $author->ID) ?></h2>
                             <div class="artikelliste__vorschau">                                
                                 <?php the_author_meta("description", $author->ID); ?>
